2456bd840e3c6abd9511ee58208c95a37515d13e
mdinh637 <mdinh637@gmail.com>
2025-12-14 04:20:34 -0800

    tower parent script
    crossbow tower child script

---
+++ 3DGroupProject/Assets/Scripts/Towers/Tower.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class Tower : MonoBehaviour
+{
+    public Transform currentEnemy;
+
+    [SerializeField] protected float attackCooldown = 1f;
+    protected float lastTimeAttacked;
+
+    [Header("Tower Setup")]
+    [SerializeField] protected Transform towerHead;
+    [SerializeField] protected float rotationSpeed = 10f;
+    [SerializeField] protected float attackRange = 3f;
+    [SerializeField] protected Transform rangeOrigin;
+    [SerializeField] protected LayerMask whatIsEnemy;
+
+
+    protected virtual void Update()
+    {
+        //if no current enemy, find a new one
+        if (currentEnemy == null)
+        {
+            currentEnemy = FindRandomEnemyWithinRange();
+            return;
+        }
+
+        //attack if able
+        if (CanAttack()) Attack();
+
+        //check if current enemy is still within range
+        if (Vector3.Distance(currentEnemy.position, rangeOrigin.position) > attackRange)
+        {
+            currentEnemy = null; //clear current enemy if out of range
+            return;
+        }
+
+        RotateTowardsEnemy(); //rotate tower head towards current enemy
+    }
+
+    protected virtual void Attack()
+    {
+    }
+
+    protected bool CanAttack()
+    {
+        //check if tower can attack based on cooldown
+        if (Time.time > lastTimeAttacked + attackCooldown)
+        {
+            lastTimeAttacked = Time.time;
+            return true;
+        }
+
+        return false;
+    }
+
+    protected Transform FindRandomEnemyWithinRange()
+    {
+        List<Transform> possibleTargets= new List<Transform>();
+        Collider[] enemiesAround = Physics.OverlapSphere(rangeOrigin.position, attackRange, whatIsEnemy); //get all enemies within range
+
+        //debug log all enemies found
+        foreach (Collider enemy in enemiesAround)
+        {
+            possibleTargets.Add(enemy.transform);
+        }
+
+        int randomIndex = Random.Range(0, possibleTargets.Count); //get random index from list of possible targets
+
+        if (possibleTargets.Count <= 0)
+        {
+            return null; //return null if no enemies found
+        }
+
+        return possibleTargets[randomIndex]; //return random enemy transform
+    }
+
+    protected virtual void RotateTowardsEnemy()
+    {
+        if (currentEnemy == null) return;
+
+        Vector3 directonToEnemy = currentEnemy.position - towerHead.position; //calc vector direction of tower aggro to current enemy
+        Quaternion lookRotation = Quaternion.LookRotation(directonToEnemy); //calc rotation needed to look at enemy
+        Vector3 rotation = Quaternion.Lerp(towerHead.rotation, lookRotation, Time.deltaTime * rotationSpeed).eulerAngles; //calc smoothed rotation
+        towerHead.rotation = Quaternion.Euler(rotation); //make tower head rotate towards enemy
+    }
+
+    protected virtual void OnDrawGizmos()
+    {
+        if (rangeOrigin == null) return;
+        
+        Gizmos.DrawWireSphere(rangeOrigin.position, attackRange);
+    }
+}
---
+++ 3DGroupProject/Assets/Scripts/Towers/Tower_Crossbow.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Tower_Crossbow : Tower
+{
+    [Header("Crossbow Tower Setup")]
+    [SerializeField] private Transform bulletPoint;
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+}
